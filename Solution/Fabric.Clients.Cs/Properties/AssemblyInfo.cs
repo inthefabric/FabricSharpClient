da78f553b806df9eb65cbb8ee2973713068d58ea
Zach Kinstner <zach@aestheticinteractive.com>
2013-08-07 15:47:55 -0400

    Updated version to 0.2.0.0. Created NuGet package.

--- Solution/Fabric.Clients.Cs/Properties/AssemblyInfo.cs
+++ Solution/Fabric.Clients.Cs/Properties/AssemblyInfo.cs
@@ -2,4 +2,5 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("Fabric.Clients.Cs")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyVersion("0.2.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyInformationalVersion("0.2.0.0.af78e66")]
