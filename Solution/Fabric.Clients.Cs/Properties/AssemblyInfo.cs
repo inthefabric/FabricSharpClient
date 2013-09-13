087acd94608d4c4f934dcc2a57a64d39b8b9b132
Zach Kinstner <zach@aestheticinteractive.com>
2013-09-13 17:02:18 -0400

    Updated version to 0.2.3.
    All 102 tests pass.

--- Solution/Fabric.Clients.Cs/Properties/AssemblyInfo.cs
+++ Solution/Fabric.Clients.Cs/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("Fabric.Clients.Cs")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyInformationalVersion("0.2.0.0.af78e66")]
+[assembly: AssemblyVersion("0.2.3.0")]
+[assembly: AssemblyInformationalVersion("0.2.3.0.edd3685")]
