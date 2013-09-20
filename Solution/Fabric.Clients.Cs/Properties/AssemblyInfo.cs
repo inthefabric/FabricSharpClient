280ca8b8c1b5cf865d895f5c9c931084f2bd5b41
Zach Kinstner <zach@aestheticinteractive.com>
2013-09-20 16:21:38 -0400

    Updated version to 0.2.4.1.

--- Solution/Fabric.Clients.Cs/Properties/AssemblyInfo.cs
+++ Solution/Fabric.Clients.Cs/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 
 [assembly: AssemblyTitle("Fabric.Clients.Cs")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyVersion("0.2.4.0")]
-[assembly: AssemblyInformationalVersion("0.2.4.0.f4202e7")]
+[assembly: AssemblyVersion("0.2.4.1")]
+[assembly: AssemblyInformationalVersion("0.2.4.1.dbb36f3")]
