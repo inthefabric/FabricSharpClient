c84b77756b4e548e7dd07e8bf4e4555f8fb80ba2
Zach Kinstner <zach@aestheticinteractive.com>
2013-09-30 14:13:03 -0400

    Updated version to 0.2.4.3.

--- Global/GlobalAssemblyVersion.cs
+++ Global/GlobalAssemblyVersion.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.2.4.2")]
-[assembly: AssemblyInformationalVersion("0.2.4.2.23cefcc")]
+[assembly: AssemblyVersion("0.2.4.3")]
+[assembly: AssemblyInformationalVersion("0.2.4.3.eef4a8a")]
